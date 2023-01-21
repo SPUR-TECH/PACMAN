5209962de77f26693896ac6ffa9af826e5a040a6
Scott Atkinson <scott.atkin79@gmail.com>
2023-01-21 13:04:58 +0000

    remove pellets on collision

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ const map = [
     ['|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|'],
     ['|', '.', '.', '.', '.', '^', '.', '.', '.', '.', '|'],
     ['|', '.', 'b', '.', '[', '5', ']', '.', 'b', '.', '|'],
-    ['|', '.', '.', '.', '.', '.', '.', '.', '.', 'p', '|'],
+    ['|', '.', '.', '.', '.', '.', '.', '.', '.', '.', '|'],
     ['4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3']
 ]
 
@@ -421,9 +421,21 @@ function animate() {
         }
     }
 
-    pellets.forEach(pellet => {
+    // touch pellets here
+    for (let i = pellets.length - 1; 0 <= i; i--) {
+        const pellet = pellets[i]
         pellet.draw()
-    })
+
+        if (
+            Math.hypot(
+                pellet.position.x - player.position.x,
+                pellet.position.y - player.position.y
+            ) <
+            pellet.radius + player.radius
+        ) {
+            pellets.splice(i, 1)
+        }
+    }
 
     boundaries.forEach((boundary) => {
         boundary.draw()
